33af537eb3c675106cea4cbcd0a29e0d836ff63f
Erick Estrada <erickdallas30@gmail.com>
2023-01-20 01:25:07 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import MovieScreen from "./components/MovieScreen";
+import Watchlist from "./components/Watchlist";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [watchList, setWatchList] = useState([]);
+  const [page, setPage] = useState([1]);
+
+  const addMovie = (movie) => setWatchList([...watchList, movie]);
+
+  const removeMovie = (movie) => {
+    const newState = watchList.filter((mov) => {
+      return mov !== movie;
+    });
+    setWatchList(newState);
+  };
+
+  function getData() {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        console.log(res.data.results);
+        setMovieList(res.data.results);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header />
+      <main>
+        <MovieScreen
+          movieList={movieList}
+          page={page}
+          setPage={setPage}
+          watchList={watchList}
+          removeMovie={removeMovie}
+        />
+        <Watchlist watchList={watchList} removeMovie={removeMovie}/>
+      </main>
     </div>
   );
 }
